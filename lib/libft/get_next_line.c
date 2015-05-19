55c49a4244133b54ede12b1df487d5000062b7ba
cglavieu <cesar.glavieux@live.fr>
2015-05-19 19:58:26 +0200

    l

--- lib/libft/get_next_line.c
+++ lib/libft/get_next_line.c
@@ -6,17 +6,17 @@
 /*   By: cglavieu <cglavieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/18 16:34:01 by cglavieu          #+#    #+#             */
-/*   Updated: 2015/05/18 19:02:45 by cglavieu         ###   ########.fr       */
+/*   Updated: 2015/05/19 19:56:46 by cglavieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <fcntl.h>
 
-static	char	*ft_line(char *s)
+static char			*ft_line(char *s)
 {
-	int			i;
-	char		*str;
+	int				i;
+	char			*str;
 
 	i = 0;
 	if (s == NULL)
@@ -29,10 +29,10 @@ static	char	*ft_line(char *s)
 	return (str);
 }
 
-static	char	*ft_stock(char *s)
+static char			*ft_stock(char *s)
 {
-	int			i;
-	char		*str;
+	int				i;
+	char			*str;
 
 	i = 0;
 	if (s == NULL)
@@ -44,38 +44,38 @@ static	char	*ft_stock(char *s)
 	return (str);
 }
 
-void			ft_del(char **buff, char **line, char **svg, int ret)
+void				ft_norme(char **buff, char **line, char **save, int ret)
 {
 	ft_strdel(buff);
-	*line = ft_line(*svg);
+	*line = ft_line(*save);
 	if (ret == 0)
-		ft_strdel(svg);
+		ft_strdel(save);
 }
 
-int				get_next_line(int const fd, char **line)
+int					get_next_line(int const fd, char **line)
 {
-	int			ret;
-	char		*tmp;
-	static char	*svg;
-	char		*buff;
+	int				ret;
+	char			*temp;
+	static char		*save;
+	char			*buff;
 
 	buff = ft_strnew(BUFF_SIZE + 1);
-	svg = (svg == NULL) ? ft_strnew(1) : svg;
+	save = (save == NULL) ? ft_strnew(1) : save;
 	if (buff == NULL || BUFF_SIZE <= 0 || line == NULL)
 		return (-1);
 	ret = 42;
-	while ((ft_strchr(svg, '\n') == NULL) && ret > 0)
+	while ((ft_strchr(save, '\n') == NULL) && ret > 0)
 	{
 		if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
 			return (-1);
-		tmp = svg;
-		svg = ft_strjoin(svg, buff);
-		ft_strdel(&tmp);
 		buff[ret] = '\0';
+		temp = save;
+		save = ft_strjoin(save, buff);
+		ft_strdel(&temp);
 	}
-	ft_del(&buff, line, &svg, ret);
-	if (ret == 0)
+	ft_norme(&buff, line, &save, ret);
+	if (ret == 0 && *line[0] == '\0')
 		return (0);
-	svg = ft_stock(svg);
+	save = ft_stock(save);
 	return (1);
 }
