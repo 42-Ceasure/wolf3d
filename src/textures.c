68dca365402f5857003fbde20eb4ba150abc88b5
cglavieu <cesar.glavieux@live.fr>
2015-06-03 22:51:10 +0200

    c'est franchement relou cette histoire de commentaires

--- src/textures.c
+++ src/textures.c
@@ -6,7 +6,7 @@
 /*   By: cglavieu <cglavieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/19 17:12:35 by cglavieu          #+#    #+#             */
-/*   Updated: 2015/06/03 22:35:58 by cglavieu         ###   ########.fr       */
+/*   Updated: 2015/06/03 22:50:05 by cglavieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ void	make_it_beautifull(t_env *w)
 {
 	SDL_Surface		*tmp;
 
-	w->load = SDL_LoadBMP("./src/textures/wool.bmp");
+	w->load = SDL_LoadBMP("./src/textures/5151.bmp");
 	tmp = SDL_ConvertSurfaceFormat(w->load, SDL_PIXELFORMAT_ARGB8888, 0);
 	SDL_FreeSurface(w->load);
 	w->sky = (Uint32 *)malloc(sizeof(Uint32) * tmp->w * tmp->h);
 	ft_memcpy(w->sky, tmp->pixels, (sizeof(Uint32) * tmp->w * tmp->h));
 	SDL_FreeSurface(tmp);
-	w->load = SDL_LoadBMP("./src/textures/plank.bmp");
+	w->load = SDL_LoadBMP("./src/textures/5107.bmp");
 	tmp = SDL_ConvertSurfaceFormat(w->load, SDL_PIXELFORMAT_ARGB8888, 0);
 	SDL_FreeSurface(w->load);
 	w->sol = (Uint32 *)malloc(sizeof(Uint32) * tmp->w * tmp->h);
 	ft_memcpy(w->sol, tmp->pixels, (sizeof(Uint32) * tmp->w * tmp->h));
 	SDL_FreeSurface(tmp);
-	w->load = SDL_LoadBMP("./src/textures/Hellokitty1.bmp");
+	w->load = SDL_LoadBMP("./src/textures/mur.bmp");
 	tmp = SDL_ConvertSurfaceFormat(w->load, SDL_PIXELFORMAT_ARGB8888, 0);
 	SDL_FreeSurface(w->load);
 	w->mur = (Uint32 *)malloc(sizeof(Uint32) * tmp->w * tmp->h);
